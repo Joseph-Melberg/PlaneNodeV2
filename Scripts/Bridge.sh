7fc753ed77d807122029bf285a1c450dec941813
Joseph-Melberg <dasbasdo@gmail.com>
2021-11-25 07:25:10 -0600

    Update Bridge.sh

--- Scripts/Bridge.sh
+++ Scripts/Bridge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-ssh -o ServerAliveInterval=60 -R 7101:localhost:22 jump@jump.centurionx.net "./KeepAlive.sh Bird"
+ssh -o ServerAliveInterval=60 -R 7101:localhost:22 jump@jump.centurionx.net "./KeepAlive.sh $HOSTNAME"
 sleep 10
 done
